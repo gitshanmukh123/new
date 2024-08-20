0057488d6029e7c462767f9bdd5bd8628b6648dd
gitshanmukh123 <yegginalakshmi369@gmail.com>
2024-08-20 19:32:15 +0530

    added nav

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import Nav from '@/Components/Nav'
+import Link from 'next/link'
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <div>
+   
+ <Nav/>
+
+  </div>
+}
+
+export default page
